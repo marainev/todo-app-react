80e16efc3f8ee6a9d6162a90bf5fd6282eddf209
marainev <mrvgllms@gmail.com>
2023-06-18 22:14:52 -0400

    apply css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import './app.scss'
+
 
 function App() {
   return (
-    <div className="App">
-      ToDo App
+    <div id="app">
+    <h1>My Todo 🗒️</h1>
+
+    <div className="container">
+      <form>
+        <h2>Add Item</h2>
+        <input type="text" id="title" placeholder="Title" class="form-control" autocomplete="off"/>
+        <button>Add</button>
+      </form>
+
+      <div className="items">
+        <h2>My Items</h2>
+      </div>
     </div>
+  </div>
   );
 }
 
